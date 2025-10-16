6fb19f05cfd518f148b7058957f50e8f3f06b416
ayla <ayla-git.barcode041@silomails.com>
2025-10-16 00:51:54 -0300

    privatebin-ayla: 7 -> 0.0.1
    Diff: https://github.com/ayla6/PrivateBin/compare/7...0.0.1
    
    Changelog: https://github.com/PrivateBin/PrivateBin/releases/tag/0.0.1

--- pkgs/pr/privatebin-ayla/package.nix
+++ pkgs/pr/privatebin-ayla/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "privatebin-ayla";
-  version = "7";
+  version = "0.0.1";
 
   src = fetchFromGitHub {
     owner = "ayla6";
     repo = "PrivateBin";
     rev = finalAttrs.version;
-    sha256 = "sha256-7lGeiRwv1iroCAofkX3Je/kui35UWv2Fpp+oWnnIzCk=";
+    sha256 = "sha256-+Ku/2gus0qqJeSuGlJPlECGXnZ6j6YdGH2QSrg5ehzE=";
   };
 
   installPhase = ''
