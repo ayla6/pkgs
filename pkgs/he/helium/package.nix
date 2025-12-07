d39141cbd4cfb2b78f24b3779257c67b801c830a
ayla <ayla-git.barcode041@silomails.com>
2025-12-07 07:13:26 -0300

    helium: 0.7.1.1 -> 0.7.2.1
    Diff: https://github.com/imputnet/helium-linux/compare/0.7.1.1...0.7.2.1

--- pkgs/he/helium/package.nix
+++ pkgs/he/helium/package.nix
@@ -161,11 +161,11 @@
 
   linux = stdenvNoCC.mkDerivation (finalAttrs: {
     inherit pname meta passthru;
-    version = "0.7.1.1";
+    version = "0.7.2.1";
 
     src = fetchurl {
       url = "https://github.com/imputnet/helium-linux/releases/download/${finalAttrs.version}/helium-${finalAttrs.version}-x86_64_linux.tar.xz";
-      hash = "sha256-8YzBMQw4cZUI01zrqI9PPbIA9DC9c6fxO+mygdBGlfs=";
+      hash = "sha256-nbg9Pf1XJ3TcOvhozOq26RMoL25sL9nzw8+IwE2HNrI=";
     };
 
     strictDeps = false;
